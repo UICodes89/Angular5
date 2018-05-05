2a0fedfd23e2742fd4f43f90a6e50bcde943ff33
Manoj <manoj.shukla@3pillarglobal.com>
2018-05-05 20:27:53 +0530

    Highlight directive

--- app1/src/app/highiliter.directive.ts
+++ app1/src/app/highiliter.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighiliter]'
 })
 export class HighiliterDirective {
 
-  constructor() { }
+  constructor(private el:ElementRef) { }
+
+    private highlight(bgColor, color){
+      this.el.nativeElement.style.color = color;
+      this.el.nativeElement.style.backgroundColor = bgColor;
+    }
+
+    @HostListener('mouseenter') onmouseHover(){
+      this.highlight("red", "#fff");
+    } 
+
+    @HostListener('mouseout') onmouseout(){
+      this.highlight("", "");
+    } 
+    
+   
 
 }
